01a54e12d0541afeaeb0938c042f94db166e7445
Levi-Wook <lwoockman@unomaha.edu>
2026-02-24 00:29:34 +0000

    words

--- Code.py
+++ Code.py
@@ -30,29 +30,18 @@ def total_agg_vol_X(volume_R, calc_fly_ash_S, calc_silica_fume_T, calc_other_SCM
     total_agg_vol_X = 27 - volume_R - calc_fly_ash_S - calc_silica_fume_T - calc_other_SCM_U - air_volume_V - water_volume_W
     return total_agg_vol_X
 
-def calc_fine_agg_Y(percent_fine_G, sg_fine_N, volume_X):
-    calc_fine_agg_Y = 62.4 * (percent_fine_G/100) * sg_fine_N * volume_X
+def calc_fine_agg_Y(percent_fine_G, sg_fine_N, total_agg_vol_X):
+    calc_fine_agg_Y = 62.4 * (percent_fine_G/100) * sg_fine_N * total_agg_vol_X
     return calc_fine_agg_Y
 
-def calc_coarse_agg_Z(percent_coarse_H, sg_coarse_O, volume_X):
-    calc_fine_agg_Y = 62.4 * (percent_coarse_H/100) * sg_coarse_O * volume_X
+def calc_coarse_agg_Z(percent_coarse_H, sg_coarse_O, total_agg_vol_X):
+    calc_fine_agg_Y = 62.4 * (percent_coarse_H/100) * sg_coarse_O * total_agg_vol_X
     return calc_fine_agg_Y
 
-def calc_other_agg_AA(percent_other_I, sg_other_P, volume_X):
-    calc_fine_agg_Y = 62.4 * (percent_other_I/100) * sg_other_P * volume_X
+def calc_other_agg_AA(percent_other_I, sg_other_P, total_agg_vol_X):
+    calc_fine_agg_Y = 62.4 * (percent_other_I/100) * sg_other_P * total_agg_vol_X
     return calc_fine_agg_Y
 
-water_Q = (cement_weight_A + fly_ash_weight_B + silica_fume_weight_C + other_weight_D)*WC_ratio_E
-R = volume_R(cement_weight_A,sg_cement_J)
-S = calc_fly_ash_S(fly_ash_weight_B,sg_fly_ash_K)
-T = calc_silica_fume_T(silica_fume_weight_C,sg_silica_fume_L)
-U = calc_other_SCM_U(other_weight_D,sg_other_SCM_M)
-V = air_volume_V(air_content_F)
-W = water_volume_W(water_Q)
-X = total_agg_vol_X(R,S,T,U,V,W)
-Y = calc_fine_agg_Y(percent_fine_G, sg_fine_N, volume_X)
-Z = calc_coarse_agg_Z(percent_coarse_H, sg_coarse_O, volume_X)
-AA = calc_other_agg_AA(percent_other_I, sg_other_P, volume_X)
 
 
 project_no = int(input("Enter project number: ")) 
@@ -86,9 +75,9 @@ W = water_volume_W(water_Q)
 
 X = total_agg_vol_X(R,S,T,U,V,W)
 
-Y = calc_fine_agg_Y(percent_fine_G, sg_fine_N, volume_X)
-Z = calc_coarse_agg_Z(percent_coarse_H, sg_coarse_O, volume_X)
-AA = calc_other_agg_AA(percent_other_I, sg_other_P, volume_X)
+Y = calc_fine_agg_Y(percent_fine_G, sg_fine_N, X)
+Z = calc_coarse_agg_Z(percent_coarse_H, sg_coarse_O, X)
+AA = calc_other_agg_AA(percent_other_I, sg_other_P, X)
 # Output: Final Weight Chart
 
 print("\n---------------------------------------------")
